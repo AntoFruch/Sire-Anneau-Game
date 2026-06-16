d6099c708ca6b3eb68c553897019282f60a8eef9
AntoFruch <antonin.fruchet@telecom-sudparis.eu>
2026-06-16 23:50:29 +0200

    add Assets.h

---
+++ src/assets/Assets.h
@@ -0,0 +1,13 @@
+#ifndef COMPONENT_BASED_ARCH_ASSETS_H
+#define COMPONENT_BASED_ARCH_ASSETS_H
+
+#include "scripts/entities/BossController.h"
+#include "scripts/entities/EnemyController.h"
+#include "scripts/entities/PlayerController.h"
+#include "scripts/Maps/LoadingZone.h"
+#include "scripts/UI/BossHealthBar.h"
+#include "scripts/UI/HUD/HealthBar.h"
+#include "scripts/UI/MainMenu.h"
+#include "scripts/UI/PauseMenu.h"
+
+#endif //COMPONENT_BASED_ARCH_ASSETS_H
